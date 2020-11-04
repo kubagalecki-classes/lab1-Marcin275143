ee2f269b7963caf83773d774c371c68f52a6824e
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-11-04 23:03:03 +0100

    Update Wektor2D.hpp

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,17 +1,20 @@
 class Wektor2D
 {
-  public:
+public:
 	double x = 0;
 	double y = 0;
+	//	double Vx = 0, Vy = 0;
 
-    Wektor2D() {};
+	Wektor2D() {};
+	//	Wektor2D() { x = 0, y = 0; };
 	Wektor2D(const double& X, const double& Y) : x(X), y(Y) {};
 
-	void setX(const double &Vx) { x = Vx; };
-	void setY(const double &Vy) { y = Vy; };
-	double getX() { cout << x << endl; };
-	double getY() { cout << y << endl; };
-	
-    friend Wektor2D operator+ (const Wektor2D& V1, const Wektor2D& V2) { double Vx = V1.x + V2.x; double Vy = V1.y + V2.y; return {Wektor2D(Vx, Vy)}; };
+	void setX(const double& Vx) { x = Vx; };
+	void setY(const double& Vy) { y = Vy; };
+	double getX() { cout << x << endl; return x; };
+	double getY() { cout << y << endl; return y; };
+
+	friend Wektor2D operator+ (const Wektor2D& V1, const Wektor2D& V2) { double Vx = V1.x + V2.x; double Vy = V1.y + V2.y; return { Wektor2D(Vx, Vy) }; };
 	friend double operator* (const Wektor2D& V1, const Wektor2D& V2) { double V = V1.x * V2.x + V1.y * V2.y; return V; };
+	friend ostream& operator<< (ostream& wskaznik, const Wektor2D& V) { return wskaznik << "(" << V.x << ", " << V.y << ")" << endl; };
 };
